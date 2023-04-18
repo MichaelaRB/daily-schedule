c8af8614e89646bfe917fa521c25c5d0d007e04c
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-18 18:48:32 -0500

    added code to display the current date in the header

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var dateEl = $('#currentDay');
+var today = dayjs();
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +22,10 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var month = today.toLocaleString('default', { month: 'long' });
+  var weekday = today.format('dddd');
+  var day = 
+  dateEl.text(today.format('dddd [, ]MM/DD')); 
 });
+console.log(month);
+console.log(today.format('hh'));
