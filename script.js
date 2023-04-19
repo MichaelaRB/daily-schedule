8f95d6018fe9abb42101abeec233be2ec82d36ce
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-18 21:20:53 -0500

    added code that applies the appropriate class based on the time of day

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var dateEl = $('#currentDay');
 var timeSlots = $('.time-block');
 var today = dayjs();
+var currentHour = today.hour();
+currentHour = Number(currentHour);
+console.log(currentHour);
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   timeSlots.on('click', 'button', function (event) {
     var btnClicked = $(event.target);
 
@@ -18,17 +13,34 @@ $(function () {
     
     localStorage.setItem(currentHour, btnClicked.parent().parent().children(".description").val());
   })
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  dateEl.text(today.format('dddd [, ]MM/DD')); 
+
+  for(var i = 9; i < 18; i++){
+    var blockId = "#hour-" + i;
+    if(i < currentHour)
+    {
+      $(blockId).addClass("past");
+      $(blockId).removeClass("present");
+      $(blockId).removeClass("future");
+    }
+    if(currentHour === i)
+    {
+      $(blockId).removeClass("past");
+      $(blockId).addClass("present");
+      $(blockId).removeClass("future");
+    }
+    if(currentHour < i) {
+      $(blockId).removeClass("past");
+      $(blockId).removeClass("present");
+      $(blockId).addClass("future");
+    }
+    $(blockId).children(".description").text(localStorage.getItem(storeId));
+  }
+  
+  for(var i = 9; i < 18; i++){
+    var blockId = "#hour-" + i;
+    var storeId = "hour-" + i;
+    $(blockId).children(".description").text(localStorage.getItem(storeId));
+  }
+
+  dateEl.text(today.format('dddd, MM/DD')); 
 });
-console.log(today.format('hh'));
