35efa314e0df1f980c13b719d6f3c91ea7d4c7ba
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-18 19:23:43 -0500

    added code to store the task in local storage by id

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var dateEl = $('#currentDay');
+var timeSlots = $('.time-block');
 var today = dayjs();
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -10,7 +11,13 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  timeSlots.on('click', 'button', function (event) {
+    var btnClicked = $(event.target);
+
+    var currentHour = btnClicked.parent().parent().attr('id');
+    
+    localStorage.setItem(currentHour, btnClicked.parent().parent().children(".description").val());
+  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -22,10 +29,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  var month = today.toLocaleString('default', { month: 'long' });
-  var weekday = today.format('dddd');
-  var day = 
   dateEl.text(today.format('dddd [, ]MM/DD')); 
 });
-console.log(month);
 console.log(today.format('hh'));
